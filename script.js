9010bb54718d2e602f5df5728f8fef66edde291c
Robson De Andrade <theairelibre@protonmail.com>
2022-04-09 12:02:57 -0300

    createElement ul addEventListener

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-devinhouse-m1-projeto1
+const ul = document.createElement('ul');
+ul.id = 'ul';
+document.querySelector('#list').append(ul);
+
+const button = document.querySelector('button');
+button.addEventListener('click', adicionaItem);
+
+function adicionaItem() {
+  const inputItem = document.getElementById('input');
+      let li = document.createElement('li');
+      li.innerText = inputItem.value;
+      document.querySelector("#ul").appendChild(li);
+}
+adicionaItem();
