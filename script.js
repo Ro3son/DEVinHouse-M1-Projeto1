916838608e61b008112d9d9a635bb61fb4fd660e
Robson De Andrade <theairelibre@protonmail.com>
2022-04-09 16:37:05 -0300

    getElementById CreateElement li

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const ul = document.createElement('ul');
 ul.id = 'ul';
 document.querySelector('#list').append(ul);
 
+// const li = ['listItem1', 'listItem2', 'listItem3', 'listItem4', 'listItem5',
+//   'listItem6', 'listItem7', 'listItem8', 'listItem9', ' listItem10'];
+
+// li.forEach(() => {
+//   let li = document.createElement('li');
+//   li.id = 'li';
+//   document.querySelector("#ul").appendChild(li);
+// });
+
 const button = document.querySelector('button');
 button.addEventListener('click', adicionaItem);
 
 function adicionaItem() {
   const inputItem = document.getElementById('input');
-      let li = document.createElement('li');
-      li.innerText = inputItem.value;
-      document.querySelector("#ul").appendChild(li);
+  let li = document.createElement('li');
+  li.innerText = inputItem.value;
+  document.querySelector("#ul").appendChild(li);
 }
-adicionaItem();
