817db3758a7b86b53e01abc72a20f3384f09e15d
Robson De Andrade <theairelibre@protonmail.com>
2022-04-11 11:43:59 -0300

    addEventListener createElement innerHTML

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-const ul = document.createElement('ul');
-ul.id = 'ul';
-document.querySelector('#list').append(ul);
-
-// const li = ['listItem1', 'listItem2', 'listItem3', 'listItem4', 'listItem5',
-//   'listItem6', 'listItem7', 'listItem8', 'listItem9', ' listItem10'];
-
-// li.forEach(() => {
-//   let li = document.createElement('li');
-//   li.id = 'li';
-//   document.querySelector("#ul").appendChild(li);
-// });
-
-const button = document.querySelector('button');
+const button = document.querySelector('#btn-form');
 button.addEventListener('click', adicionaItem);
 
 function adicionaItem() {
-  const inputItem = document.getElementById('input');
-  let li = document.createElement('li');
-  li.innerText = inputItem.value;
-  document.querySelector("#ul").appendChild(li);
+
+    const inputItem = document.getElementById('input-form');
+
+    const li = document.createElement("li");
+    li.id = 'listItem';
+    li.innerHTML = `<input id='check' type='checkbox'/>
+    <label>${inputItem.value}</label>
+    <button id="btn-xmark"><i class="fa-solid fa-rectangle-xmark"></i></button>`;
+    document.querySelector('ul').append(li);
+
+    // let checkbox = ('#check:checked') ?
+    //     document.querySelector('label').style.textDecoration = 'line-through' :
+    //     document.querySelector('label').style.textDecoration = 'none';
 }
+
+
