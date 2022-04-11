a170d4740b2b666fcdc8302cb0d837a8361694ff
Robson De Andrade <theairelibre@protonmail.com>
2022-04-11 15:35:39 -0300

    change innerHTML label for span

--- script.js
+++ script.js
@@ -4,17 +4,13 @@ button.addEventListener('click', adicionaItem);
 function adicionaItem() {
 
     const inputItem = document.getElementById('input-form');
-
     const li = document.createElement("li");
     li.id = 'listItem';
-    li.innerHTML = `<input id='check' type='checkbox'/>
-    <label>${inputItem.value}</label>
+    li.innerHTML = `<input id='check' type='checkbox'/><span>${inputItem.value}</span>
     <button id="btn-xmark"><i class="fa-solid fa-rectangle-xmark"></i></button>`;
     document.querySelector('ul').append(li);
-
-    // let checkbox = ('#check:checked') ?
-    //     document.querySelector('label').style.textDecoration = 'line-through' :
-    //     document.querySelector('label').style.textDecoration = 'none';
+    document.querySelector('ul').style.backgroundColor = 'lightslategrey';
+    document.getElementById('container2').style.backgroundColor = '#2a2d2f';
 }
 
 
