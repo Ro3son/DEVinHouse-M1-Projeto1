3f831510eee55b6f4ebbf426c4c6901896f8d665
Robson De Andrade <theairelibre@protonmail.com>
2022-04-12 12:01:26 -0300

    remove() method parentElement

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const button = document.querySelector('#btn-form');
-button.addEventListener('click', adicionaItem);
+button.addEventListener('click', () => {
+    adicionaLista();
+});
 
-function adicionaItem() {
+const adicionaLista = () => {
 
     const inputItem = document.getElementById('input-form');
+
     const li = document.createElement("li");
     li.id = 'listItem';
-    li.innerHTML = `<input id='check' type='checkbox'/><span>${inputItem.value}</span>
-    <button id="btn-xmark"><i class="fa-solid fa-rectangle-xmark"></i></button>`;
-    document.querySelector('ul').append(li);
+    li.innerHTML = `<input id='check' type='checkbox'/>
+    <span>${inputItem.value}</span>
+    <button id="btn-xmark">
+        <i class="fa-solid fa-rectangle-xmark"></i>
+    </button>`;
+    document.querySelector('ul').appendChild(li);
     document.querySelector('ul').style.backgroundColor = 'lightslategrey';
-    document.getElementById('container2').style.backgroundColor = '#2a2d2f';
+
+    rabiscaLista();
+    deletaLista();
+
 }
-
-
+const rabiscaLista = () => {
+    const checked = document.querySelectorAll('#check');
+    checked.forEach((check) => {
+        check.addEventListener('click', function() {
+            document.querySelector('span').style.textDecoration = 'line-through';
+        });
+    })
+        
+};
+const deletaLista = () => {
+    const buttons = document.querySelectorAll('#btn-xmark');
+    for (let btn of buttons) {
+        btn.addEventListener('click', function() {
+            this.parentElement.remove();
+        });
+    };
+};
