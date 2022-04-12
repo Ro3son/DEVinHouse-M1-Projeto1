2ba685eba775146580a1231535fd203c215947fd
Robson De Andrade <theairelibre@protonmail.com>
2022-04-12 15:48:16 -0300

    addEventListener delete for of

--- script.js
+++ script.js
@@ -9,27 +9,33 @@ const adicionaLista = () => {
 
     const li = document.createElement("li");
     li.id = 'listItem';
-    li.innerHTML = `<input id='check' type='checkbox'/>
-    <span>${inputItem.value}</span>
+    li.innerHTML = `<input id='checked' type='checkbox'/>
+    <label>${inputItem.value}</label>
     <button id="btn-xmark">
         <i class="fa-solid fa-rectangle-xmark"></i>
-    </button>`;
+    </button>
+
+    <div class="container-confirm">
+        <input id="inputHidden" type="number" />
+        <button id="btnHidden" type="submit">Confirmar Valor</button>
+    </div>
+    `;
     document.querySelector('ul').appendChild(li);
     document.querySelector('ul').style.backgroundColor = 'lightslategrey';
-
-    rabiscaLista();
+    
+    // popUp();
+    // rabiscaLista();
     deletaLista();
 
 }
-const rabiscaLista = () => {
-    const checked = document.querySelectorAll('#check');
-    checked.forEach((check) => {
-        check.addEventListener('click', function() {
-            document.querySelector('span').style.textDecoration = 'line-through';
-        });
-    })
-        
-};
+// const rabiscaLista = () => {
+//     const checked = document.querySelectorAll('#check');
+//     for(let check of checked) {
+//         check.addEventListener('click', function() {
+//             document.querySelector('span').style.textDecoration = 'line-through';
+//         });
+//     }
+// };
 const deletaLista = () => {
     const buttons = document.querySelectorAll('#btn-xmark');
     for (let btn of buttons) {
@@ -38,3 +44,16 @@ const deletaLista = () => {
         });
     };
 };
+// const popUp = () => {
+//     const checks = document.querySelectorAll('#checked');
+//     for(let check of checks) {
+//         check.addEventListener('click', function() {
+//             if (this.checked) {
+//                 document.querySelector('.container-confirm').style.display = 'flex';
+//             } else {
+//                  document.querySelector('.container-confirm').style.display = 'none';
+//             }
+            
+//         });
+//     }
+// };
